3f25feeeabc51d3a08a97847c43a0cd0dac0f2c6
ngard <ngard@roadstar>
2019-04-21 17:43:43 +0900

    Change P constant of PID controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -13,7 +13,7 @@ class Controller(object):
         # TODO: Implement
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
 
-        kp = 0.3
+        kp = 0.2
         ki = 0.1
         kd = 0.
         mn = 0.   # Minimum throttle value
